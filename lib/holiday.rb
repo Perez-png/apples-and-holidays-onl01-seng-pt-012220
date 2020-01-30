cca47f3208b1b93585685c12d0a7572ed3698705
Xavier Perez <xperez007@gmail.com>
2020-01-30 19:22:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,4 +1,12 @@
-require 'pry'
+def 
+  second_supply_for_fourth_of_july
+  (holiday_supplies)
+    holiday_supplies[:summer]
+  [:fourth_of_july] [1]
+  end
+  
+  
+  require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
